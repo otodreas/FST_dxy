c81176db90c36ab56529dcd56ac3fa18a94d1dd6
Oliver Todreas <otodreas@gmail.com>
2026-02-14 17:05:42 +0100

    clarify filtering pipeline

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -15,18 +15,19 @@ VCF_Z_F="$CWD/ProjTaxaFilt.vcf.gz"
 # Filter data
   # Remove outgroup (Naxos2)
   # Keep biallelic SNPs only
-  # Filter by genotype quality (GQ>30)
-  # Filter by sample depth 
-  # Filter by quality (GQ>30) and depth (2.5-50)
-  # Keep sites with <10% missing data
+  # Filter by quality by genotypes (GQ>30)
+  # Filter by sample depth by genotypes (2.5<DP<70)
+  # Filter by mean depth by sites (5<meanDP<50)
+  # Filter by missingness by sites (exclude missing>90%)
+  # Recode output to VCF with all metadata
+  # Pipe output into bcftools to encode in a bgzipped file
 vcftools --gzvcf "$VCF_Z" \
   --remove-indv Naxos2 \
-  --remove-indels \
+  --remove-indels --min-alleles 2 --max-alleles 2 \
   --minGQ 30 \
   --minDP 2.5 --maxDP 70 \
-  --min-meanDP 2.5 --max-meanDP 50 \
-  --max-missing 0.9 \
-  --min-alleles 2 --max-alleles 2 \
+  --min-meanDP 5 --max-meanDP 50 \
+  --max-missing 0.1 \
   --recode --recode-INFO-all \
   --stdout | \
   bcftools view -O z -o "$VCF_Z_F"
